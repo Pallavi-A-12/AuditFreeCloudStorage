f22f3f4d55750084fed3dfde114be4e56853735e
Pallavi-A-12 <pallavibhavani1@gmail.com>
2026-06-08 13:39:30 +0530

    Initial project upload

---
+++ src/auditfreecloudstorage/Main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package auditfreecloudstorage;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+/**
+ * @author Elcot
+ */
+public class Main {
+    public static void main(String[] args) 
+    {        
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JDialog.setDefaultLookAndFeelDecorated(true);
+        try
+        {                    			
+            UIManager.setLookAndFeel("com.jtattoo.plaf.bernstein.BernsteinLookAndFeel");
+                                                
+            LoginPage hp=new LoginPage();
+            hp.setTitle("Login Page");
+            hp.setVisible(true);
+            hp.setResizable(false);                         
+	}
+	catch (Exception ex)
+	{            
+            //System.out.println(ex);
+	}   
+    }
+}
+
+
